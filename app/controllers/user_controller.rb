436ab07e00fafdbd580a25baacc832128e37b180
SHIMOMURA Sho <graueule@gmail.com>
2013-04-05 11:14:27 +0900

    create new dm room if the room has been deleted

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,10 +6,13 @@ class UserController < ApplicationController
     user_ids = users.map {|user| user._id.to_s}
 
     dmroom = DirectMessageRoom.find_or_create_by(:users => user_ids)
-    unless dmroom.room_id
-      title = users.map{|user| user.name}.join(" and ")
-      Room.find_or_create_by()
+    if dmroom.room_id
+      room = Room.where(:_id => dmroom.room_id).first
+      dmroom.room_id = nil if room.deleted
+    end
 
+    if dmroom.room_id.nil?
+      title = users.map{|user| user.name}.join(" and ")
       room = Room.new(:title => title, :user => user_ids[0], :member_ids => [user_ids[1]], :updated_at => Time.now, :is_public => false)
       room.save
       dmroom.room_id = room._id
